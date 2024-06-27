82bb08d5386c799ada17ad249f1855e5a28f80f3
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-27 21:12:34 +0600

    Refactored the main file

--- main.py
+++ main.py
@@ -24,38 +24,51 @@ def get_articles_and_save(url, adapter_class, website_name):
 
     return articles
 
+def scrape_multiple_pages(config):
+    for i in range(1, config['total_pages']+1):
+        url = config['base_url'].format(i)
+        get_articles_and_save(url, config['adapter'], config['website_name'])
+
+site_config = [
+    {
+        'base_url': 'https://news.ycombinator.com/?p={}',
+        'adapter': hackernews_adapter.HackerNews,
+        'website_name': 'hackernews',
+        'total_pages': 5
+    },
+    {
+        'base_url': 'https://techcrunch.com/category/startups/page/{}',
+        'adapter': techcrunch_adapter.TechCrunch,
+        'website_name': 'techcrunch',
+        'total_pages': 5
+    },
+    {
+        'base_url': 'https://www.thedailystar.net/tech-startup/latest?page={}',
+        'adapter': dailystarnews_adapter.DailyStarNews,
+        'website_name': 'dailystar',
+        'total_pages': 5
+    },
+    {
+        'base_url': 'https://www.theverge.com/tech/archives/{}',
+        'adapter': vergenews_adapter.VergeNews,
+        'website_name': 'vergenews',
+        'total_pages': 5
+    },
+    {
+        'base_url': 'https://www.techradar.com/computing/software/artificial-intelligence/{}',
+        'adapter': techradarnews_adapter.RadarNews,
+        'website_name': 'techradar',
+        'total_pages': 5
+    }
+]
 
 if __name__ == "__main__":
     total_pages = 6
 
     print("Started Scraping")
 
-    for i in range(1, total_pages):
-        hackernews_url = f"https://news.ycombinator.com/?p={i}"
-        get_articles_and_save(
-            hackernews_url, hackernews_adapter.HackerNews, "hackernews"
-        )
-
-
-    for i in range(1, total_pages):
-        tech_crunch_url = f"https://techcrunch.com/category/startups/page/{i}"
-        get_articles_and_save(
-            tech_crunch_url, techcrunch_adapter.TechCrunch, "techcrunch"
-        )
-
-    for i in range(1, 50):
-        daily_star_url = f"https://www.thedailystar.net/tech-startup/latest?page={i}"
-        get_articles_and_save(
-            daily_star_url, dailystarnews_adapter.DailyStarNews, "dailystar"
-        )
-
-    for i in range(1, 15):
-        verge_news_url = f"https://www.theverge.com/tech/archives/{i}"
-        get_articles_and_save(verge_news_url, vergenews_adapter.VergeNews, "vergenews")
-    
-    for i in range(1, 6):
-        tech_radar_url = f"https://www.techradar.com/computing/software/artificial-intelligence/{i}"
-        get_articles_and_save(tech_radar_url, techradarnews_adapter.RadarNews, "techradar")
+    for data in site_config:
+        scrape_multiple_pages(data)
 
     reddit_url = "https://www.reddit.com/r/technews/"
     get_articles_and_save(reddit_url, reddit_adapter.RedditNews, "redditnews")
