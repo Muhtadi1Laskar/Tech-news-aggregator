d18f7489af44f6a167db0737b7de2b1b434c4881
Muhtadi1Laskar <40865352+Muhtadi1Laskar@users.noreply.github.com>
2026-01-29 17:08:05 +0600

    Update User-Agent header in fetch_function

--- fetch.py
+++ fetch.py
@@ -28,7 +28,12 @@ def fetch_json(url):
 
 def fetch_function(url):
     header = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/122.0 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "bn-BD,bn;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Connection": "keep-alive",
     }
 
     # time.sleep(random.uniform(1, 3))
