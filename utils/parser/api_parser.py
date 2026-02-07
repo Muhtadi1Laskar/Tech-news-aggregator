d024fe58bc3def792c3852d01be5764250a9ab8d
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2026-02-07 19:16:41 +0600

    Refactored the code

--- utils/parser/api_parser.py
+++ utils/parser/api_parser.py
@@ -17,28 +17,20 @@ def parse_api(json_data, name, new_type, selectors):
     return articles
 
 
-selectors = {
-    "story": "stories",
-    "title": "title",
-    "link": "news_slug",
-    "publish_date": "cat_name.created_at",
-}
-
-
 def extract_nested_item(key_string, data):
     if not key_string or not isinstance(data, dict):
         return None
-    
+
     keys = key_string.split(".")
     current = data
 
     for key in keys:
         if not isinstance(current, dict):
             return None
-        
+
         if not key in current:
             return None
-        
+
         current = current[key]
 
     return current
